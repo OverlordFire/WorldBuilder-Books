d2b1432a924f0df4b17982d228f33401b5376e7c
OverlordFire <154038232+OverlordFire@users.noreply.github.com>
2026-06-20 14:55:34 -0300

    Update login and registration fetch URLs

--- Js/auth_script.js
+++ Js/auth_script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       console.log("Enviando...");
-      const res  = await fetch("/login", {
+      const res  = await fetch("https://worldbuilder-b.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       console.log("Enviando...");
-      const res  = await fetch("/register", {
+      const res  = await fetch("https://worldbuilder-b.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password })
